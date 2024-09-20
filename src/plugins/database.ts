5e110847c00baca07e3ff1bb3bf3a85ec46659d4
Aini <3258853+aini-naire@users.noreply.github.com>
2024-09-20 10:45:11 -0300

    chore: bun changes

--- src/plugins/database.ts
+++ src/plugins/database.ts
@@ -1,17 +1,17 @@
 import fp from "fastify-plugin";
-import { BetterSQLite3Database, drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
+import { BunSQLiteDatabase, drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from 'bun:sqlite';
 import { FastifyInstance, FastifyPluginAsync } from "fastify";
 
 import * as schema from "@blog/database/schema.js";
 
 declare module "fastify" {
   interface FastifyInstance {
-    db: BetterSQLite3Database<typeof schema>;
+    db: BunSQLiteDatabase<typeof schema>;
   }
 }
 
-const sqlite = new Database("sqlite.db");
+const sqlite = new Database("data/sqlite.db");
 const database = drizzle(sqlite, { schema });
 
 const databasePlugin = fp(
