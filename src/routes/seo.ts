38e4cd2b06db5eac20d5a2248afde5bc32b27af3
Aini <3258853+aini-naire@users.noreply.github.com>
2024-09-30 08:42:26 -0300

    feat: additional SEO routes

--- src/routes/seo.ts
+++ src/routes/seo.ts
@@ -3,6 +3,17 @@ import BlogService from "@blog/services/blog.js";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import type { FastifyInstance } from "fastify";
 
+function renderHTML(title: string, desc: string) {
+    return `<!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="`+ desc + `">
+    <title>emi's blog - `+ title + `</title>
+  </head>
+</html>`
+}
+
 export default async function postRoutes(fastify: FastifyInstance) {
     const server = fastify.withTypeProvider<TypeBoxTypeProvider>();
 
@@ -18,17 +29,41 @@ export default async function postRoutes(fastify: FastifyInstance) {
 
             if (post) {
                 response.type("text/html")
-                response.send(`<!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="description" content="`+ post.tagline + `">
-    <title>emi's blog - `+ post.title + `</title>
-  </head>
-</html>`);
+                response.send(renderHTML(post.title, post.tagline));
             } else {
                 response.status(404).send({ message: "post_not_found" });
             }
+        },
+    });
+
+    server.get("/tag/:tagURL", {
+        schema: {
+            tags: ["PUBLIC"],
+            response: {
+            }
+        },
+        handler: async (request, response) => {
+            const { tagURL } = request.params;
+            const tag = await BlogService.getTag(tagURL);
+
+            if (tag) {
+                response.type("text/html")
+                response.send(renderHTML(tag.title, tag.tagline));
+            } else {
+                response.status(404).send({ message: "tag_not_found" });
+            }
+        },
+    });
+
+    server.get("/", {
+        schema: {
+            tags: ["PUBLIC"],
+            response: {
+            }
+        },
+        handler: async (request, response) => {
+            response.type("text/html");
+            response.send(renderHTML("main", "Ramblings about tech, fashion, photography, etc."));
         },
     });
 }
